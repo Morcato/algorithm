a83eba0b6053b81bfa6d266e9674b2956682e40c
Shawn <545814826@qq.com>
2021-03-05 14:08:05 +0800

    完成LeetCode第42题

--- src/main/java/com/morcat/leetcode/Q42.java
+++ src/main/java/com/morcat/leetcode/Q42.java
@@ -62,4 +62,35 @@ public class Q42 {
         }
     }
 
+    /**
+     * 双指针法
+     */
+    class TwoPoints {
+        public int trap(int[] height) {
+            int result = 0;
+            int left = 0;
+            int right = height.length - 1;
+            int leftMax = 0;
+            int rightMax = 0;
+            while (left < right) {
+                leftMax = Math.max(leftMax, height[left]);
+                rightMax = Math.max(rightMax, height[right]);
+                if (leftMax < rightMax) {
+                    if (leftMax > height[left]) {
+                        result = leftMax - height[left] + result;
+
+                    }
+                    left++;
+                } else {
+                    if (rightMax > height[right]) {
+                        result = rightMax - height[right] + result;
+
+                    }
+                    right--;
+                }
+            }
+            return result;
+        }
+    }
+
 }
