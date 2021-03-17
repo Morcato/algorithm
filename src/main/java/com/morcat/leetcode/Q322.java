d52b82a269427639156f6c6776ca489203e68300
Shawn <545814826@qq.com>
2021-03-17 23:43:50 +0800

    完成LeetCode第322题(dp数组迭代解法)

--- src/main/java/com/morcat/leetcode/Q322.java
+++ src/main/java/com/morcat/leetcode/Q322.java
@@ -1,5 +1,6 @@
 package com.morcat.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,7 +45,7 @@ public class Q322 {
     }
 
     /**
-     * 使用备忘录优化,暴力解法
+     * 使用备忘录优化(自顶而下)
      */
     class MemorySolution {
 
@@ -82,5 +83,29 @@ public class Q322 {
         }
 
     }
+
+    /**
+     * dp 数组的迭代解法 (自底而上)
+     */
+    class DpTableSolution {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp,Integer.MAX_VALUE);
+            dp[0] = 0;
+            for(int i = 0 ; i<=amount ; i++){
+                for(int coin:coins){
+                    if(i-coin>=0 && dp[i - coin] != Integer.MAX_VALUE){
+                        dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+                    }
+                }
+            }
+            if(dp[amount] == Integer.MAX_VALUE){
+                return -1;
+            }else{
+                return dp[amount];
+            }
+        }
+    }
+
 
 }
