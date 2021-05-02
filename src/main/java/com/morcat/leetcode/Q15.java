903dfe23040203f3e1d8fa8999fb5d33ceaf7f9c
Shawn <545814826@qq.com>
2021-05-02 12:11:28 +0800

    完成LeetCode第15题

---
+++ src/main/java/com/morcat/leetcode/Q15.java
@@ -0,0 +1,78 @@
+package com.morcat.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ *  Q15_三数之和
+ *
+ *  给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 示例 2：
+ *
+ * 输入：nums = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：[]
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ *
+ *  @author shenzixing
+ *  @since 2021-05-02
+ */
+public class Q15 {
+
+    /**
+     * 解题思路: 先排序,再使用双指针法
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int L = i + 1;
+            int R = nums.length - 1;
+            while (L < R) {
+                int val = nums[i] + nums[L] + nums[R];
+                if (val == 0) {
+                    result.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) {
+                        L++;
+                    }
+                    while (L < R && nums[R] == nums[R - 1]) {
+                        R--;
+                    }
+                    L++;
+                    R--;
+                } else if (val > 0) {
+                    R--;
+                } else if (val < 0) {
+                    L++;
+                }
+
+            }
+        }
+        return result;
+    }
+}
