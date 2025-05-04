fa1a3b7702419948ccd48b328c04cd4914e8c31e
shenzixing <shenzixing@meituan.com>
2025-05-04 13:51:05 +0800

    完成leetcode第88题

--- src/main/java/com/morcat/leetcode/Q88.java
+++ src/main/java/com/morcat/leetcode/Q88.java
@@ -1,33 +1,32 @@
 package com.morcat.leetcode;
 
 /**
- *
- *  Q88_合并两个有序数组
- *
- *  给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
- *
+ * Q88_合并两个有序数组
+ * <p>
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * <p>
  * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
  * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
  * 输出：[1,2,2,3,5,6]
- *
+ * <p>
  * 示例 2：
  * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
  * 输出：[1]
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * nums1.length == m + n
  * nums2.length == n
  * 0 <= m, n <= 200
  * 1 <= m + n <= 200
  * -109 <= nums1[i], nums2[i] <= 109
  *
- *  @author shenzixing
- *  @since 2021-04-05
+ * @author shenzixing
+ * @since 2021-04-05
  */
 public class Q88 {
 
@@ -60,6 +59,35 @@ public class Q88 {
         for (int i = 0; i < m + n; i++) {
             nums1[i] = sort[i];
         }
+    }
+
+    /**
+     * 从后往前填充,进行原地操作
+     * 因为 nums1 的后面有足够的空间，所以我们可以从后往前比较，把较大的数放到 nums1 的末尾，这样不会覆盖还没处理的元素。
+     */
+    public void merge_v2(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m - 1;
+        int index2 = n - 1;
+
+        int index = m + n - 1;
+
+        while (index1 >= 0 || index2 >= 0) {
+            if (index1 < 0) {
+                nums1[index] = nums2[index2];
+                index2--;
+            } else if (index2 < 0) {
+                nums1[index] = nums1[index1];
+                index1--;
+            } else if (nums1[index1] >= nums2[index2]) {
+                nums1[index] = nums1[index1];
+                index1--;
+            } else if (nums1[index1] < nums2[index2]) {
+                nums1[index] = nums2[index2];
+                index2--;
+            }
+            index--;
+        }
+
     }
 
 }
